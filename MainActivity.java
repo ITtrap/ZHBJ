284e47c11d0b0fb23b752cc1c803f81523f0a1f5
xuchen <woaixuchen1314>
2016-08-15 13:44:32 +0800

    toast

--- MainActivity.java
+++ MainActivity.java
@@ -3,6 +3,7 @@ package com.example.xuchen.zhbj;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.widget.Toast;
 
 /**
  * Created by xuchen on 2016/8/10.
@@ -12,6 +13,6 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
         setContentView(R.layout.activity_main);
-        //Toast.makeText()
+        Toast.makeText(MainActivity.this,"哈哈",Toast.LENGTH_SHORT).show();
     }
 }
